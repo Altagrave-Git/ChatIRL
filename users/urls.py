8f337c3ce1c318d4423ccb331f0f8f0f3d55a9d1
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-04 20:08:16 -0600

    Add profile url to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from users import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('u/<str:username>/', views.profile_view, name="profile")
 ]

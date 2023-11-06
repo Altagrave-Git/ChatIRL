a3e71d74aec2a462712568ff6d7d2c1cc31f43bb
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-05 20:19:00 -0700

    Add private chat websocket url pattern

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
+    re_path(r'ws/room/(?P<room_name>.*)/$', consumers.ChatRoomConsumer.as_asgi()),
+    re_path(r'ws/private/(?P<username>.*)/$', consumers.PrivateChatConsumer.as_asgi()),
 ]

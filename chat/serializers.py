ecaa2ce29020b4a05ab96e53b5ec6510fc875c4e
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-05 20:19:22 -0700

    Add private chat serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from chat.models import ChatRoom, ChatMessage
+from chat.models import ChatRoom, ChatMessage, PrivateChat, PrivateMessage
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
-        fields = ['id', 'title', 'slug', 'last_message', 'timestamp', 'users']
+        fields = ['id', 'title', 'slug', 'description', 'last_message', 'timestamp', 'users']
 
 
 class ChatMessageSerializer(serializers.ModelSerializer):
@@ -14,4 +14,25 @@ class ChatMessageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ChatMessage
+        fields = ['id', 'text', 'timestamp', 'user', 'room', 'username', 'color']
+
+
+class PrivateChatSerializer(serializers.ModelSerializer):
+    usernames = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PrivateChat
+        fields = ['id', 'last_message', 'timestamp', 'users', 'usernames']
+
+    def get_usernames(self, obj):
+        usernames = [user.username for user in obj.users.all()]
+        return usernames
+
+
+class PrivateMessageSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    color = serializers.CharField(source='user.color', read_only=True)
+
+    class Meta:
+        model = PrivateMessage
         fields = ['id', 'text', 'timestamp', 'user', 'room', 'username', 'color']

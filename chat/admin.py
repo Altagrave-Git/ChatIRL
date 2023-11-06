e33c094c780fb2dfa8ef4026462ee3cea49d41c9
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-05 20:17:37 -0700

    Add private room to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from chat.models import ChatRoom, ChatMessage
+from chat.models import ChatRoom, ChatMessage, PrivateChat, PrivateMessage
 
 
 admin.site.register(ChatRoom)
 admin.site.register(ChatMessage)
+admin.site.register(PrivateChat)

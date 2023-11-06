5ee3c9b8433b5420391c231d1b624377934c6150
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-05 20:19:35 -0700

    Add private chat llist and detail views

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('create/', views.create_view, name='create_room'),
     path('room/<slug:slug>/', views.room_view, name='room'),
+    path('private/', views.private_list_view, name='private_list'),
+    path('private/<str:username>/', views.private_chat_view, name='private')
 ]
